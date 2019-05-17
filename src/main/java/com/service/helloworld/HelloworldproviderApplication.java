303c0a0e5cce96c204e59087480d7dab8475cca2
laijins <710921398@qq.com>
2019-05-17 13:46:00 +0800

    首次导入

--- src/main/java/com/service/helloworld/HelloworldproviderApplication.java
+++ src/main/java/com/service/helloworld/HelloworldproviderApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableServiceComb
 public class HelloworldproviderApplication {
+
     public static void main(String[] args) {
          SpringApplication.run(HelloworldproviderApplication.class,args);
     }
